3c006b0115aa87ac7709f6fb73f2fa11104f163f
Hiago Henrique <hiago_h3@hotmail.com>
2022-09-29 22:33:10 -0300

    Alterando background image
    Mudando a imagem de fundo de acordo com o horario.

--- script.js
+++ script.js
@@ -25,5 +25,31 @@ function fixZero(time) {
     return time < 10? `0${time}` : time; //If simplificado;
 } //Para adicionar um 0 quando a hora, minuto ou segundo for menor do que 10;
 
+
 setInterval(updateClock, 1000);
 updateClock();
+
+let now = new Date(); //Pegar data completa
+let hour = now.getHours(); //Pegar as horas
+
+if (hour >=0 && hour <6) {
+    //boa madrugada
+    document.querySelector('body').style.background = '#0d0e12'
+    document.querySelector('h3').innerHTML="Boa madrugada!";
+
+}else if(hour >=6 && hour <12){
+    //bom dia
+    document.querySelector('body').style.background = '#7d92b1'
+    document.querySelector('h3').innerHTML="Bom dia!";
+}else if (hour>=12 && hour <18){
+    //boa tarde
+    document.querySelector('body').style.background = '#ff6002'
+    document.querySelector('h3').innerHTML="Boa tarde!";
+}else{
+    //boa noite
+    document.querySelector('body').style.backgroundColor = '#000000'
+    document.querySelector('body').style.backgroundImage = 'url(images/noite.jpg)'
+    document.querySelector('h3').innerHTML="Boa noite!";
+}
+
+
